95892ab298671ec1b2766030253deeecf8e36efc
Prithvi101 <pruthvik41@hotmail.com>
2023-04-04 01:28:30 +0530

    SmallUpdate

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "";
+  "https://github.com/Prithvi101/portfolio-prithvi/blob/master/src/Assets/cv_prithvi_updated.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
