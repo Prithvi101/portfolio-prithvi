43ca74bb5b97ec9149aac5867febb4aa46e143d3
Prithvi101 <pruthvik41@hotmail.com>
2024-11-21 01:19:51 +0530

    fix: added base path to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/portfolio-prithvi",
+  assetPrefix: "/portfolio-prithvi/", // Ensures static assets like images load correctly
+
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
