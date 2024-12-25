aedc3cdad73fa06c5c4251277bb5e1e30ace447d
Prithvi101 <pruthvik41@hotmail.com>
2024-12-25 15:52:41 +0530

    chore: updated new experience

--- src/database/Experience.ts
+++ src/database/Experience.ts
@@ -1,15 +1,22 @@
 export const Experiences = [
   {
+    name: "Goemon",
+    role: "Front-End Developer",
+    duration: "Dec 2024 - Present",
+    mode: "Remote",
+    location: "Dover, Delaware, United States",
+  },
+  {
     name: "Bex Money ",
     role: "Front-End Developer",
-    duration: "July 2023 - Present",
+    duration: "August 2023 - Dec 2024 ",
     mode: "Remote",
     location: "London, United Kingdom",
   },
   {
     name: "Inphynous Technology Solutions",
-    role: "Front-End Developer Intern",
-    duration: "March 2023 - July 2023",
+    role: "Web Developer",
+    duration: "Dec 2022 - July 2023",
     mode: "Hybrid",
     location: "Aurangabad",
   },
