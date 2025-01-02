77a3777f1171ab938ffa5d9ae56fbf9fec060021
Prithvi <63555208+Prithvi101@users.noreply.github.com>
2025-01-02 12:36:41 +0530

    Update Experience.ts

--- src/database/Experience.ts
+++ src/database/Experience.ts
@@ -1,12 +1,5 @@
 export const Experiences = [
   {
-    name: "Goemon",
-    role: "Front-End Developer",
-    duration: "Dec 2024 - Present",
-    mode: "Remote",
-    location: "Dover, Delaware, United States",
-  },
-  {
     name: "Bex Money ",
     role: "Front-End Developer",
     duration: "August 2023 - Dec 2024 ",
