9d635987701fdcbea9bf935efabab4fcce250725
Prithvi101 <pruthvik41@hotmail.com>
2025-09-16 17:45:41 +0530

    fix: removed animated cursor ring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,13 @@ export default function Home() {
   return (
     <main className=" text-[#fff] ">
       <AnimatedCursor
-        innerSize={8}
-        outerSize={50}
+        innerSize={13}
+        outerSize={35}
         innerScale={1}
-        outerScale={2}
+        outerScale={1.7}
         outerAlpha={0}
         showSystemCursor={true}
-        innerStyle={{
-          backgroundColor: "#FFFF00",
-          zIndex: 1000,
-        }}
-        outerStyle={{
-          border: "3px solid #fff",
-          zIndex: 10000,
-        }}
+        innerStyle={{ backgroundColor: "#eab308", mixBlendMode: "exclusion" }}
       />
       <Hero></Hero>
       <Skills></Skills>
